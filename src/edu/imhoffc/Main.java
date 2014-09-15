051cfece1b34b649d78223acc45abd4dcf228a77
Charlie Imhoff <imhoffc@carleton.edu>
2014-09-15 12:54:43 -0500

    Added some basic, conversational code.

--- src/edu/imhoffc/Main.java
+++ src/edu/imhoffc/Main.java
@@ -1,8 +1,19 @@
 package edu.imhoffc;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Yo.");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = in.nextLine();
+
+        System.out.println("Hey "+name);
+        System.out.println();
+        System.out.println();
     }
+
 }
