0b4642a55f699c7e29a6492d394da64573bad8ce
Charlie Imhoff <imhoffc@carleton.edu>
2014-09-19 12:29:42 -0500

    Finished code for the UnicodeCharTest. Handling bytes was tricky, so we may need to double check that test method if we are having issues.

--- src/edu/imhoffc/EncodingHelperCharTest.java
+++ src/edu/imhoffc/EncodingHelperCharTest.java
@@ -43,25 +43,42 @@ public class EncodingHelperCharTest {
 
     @Test
     public void testToUTF8Bytes() throws Exception {
+        byte[] latinCapsLBytes = {b(76)};
+            //just 76 because it's ascii
+        assertArrayEquals(latinCapsLBytes, latinCapsL.toUTF8Bytes());
 
+        byte[] latinSmallLetterZWithDescenderBytes = {b(226), b(177), b(172)};
+        assertArrayEquals(latinSmallLetterZWithDescenderBytes, latinSmallLetterZWithDescender.toUTF8Bytes());
+
+        byte[] musicalSymbolQuarterRestBytes = {b(240), b(157), b(132), b(189)};
+        assertArrayEquals(musicalSymbolQuarterRestBytes, musicalSymbolQuarterRest.toUTF8Bytes());
+    }
+
+    //a simple helper method for all the byte downcasting down in the above test.
+    private byte b(int i) {
+        return (byte) i;
     }
 
     @Test
     public void testToCodePointString() throws Exception {
-        assertTrue("U+004C".equalsIgnoreCase(latinCapsL.toCodePointString()));
-        assertTrue("U+2C6C".equalsIgnoreCase(latinSmallLetterZWithDescender.toCodePointString()));
-        assertTrue("U+1D13D".equalsIgnoreCase(musicalSymbolQuarterRest.toCodePointString()));
+        assertEquals("U+004C",latinCapsL.toCodePointString());
+        assertEquals("U+2C6C",latinSmallLetterZWithDescender.toCodePointString());
+        assertEquals("U+1D13D",musicalSymbolQuarterRest.toCodePointString());
     }
 
     @Test
     public void testToUTF8StringWithoutQuotes() throws Exception {
-
+        //an extra backslash in included in these String literals
+        // otherwise Java would attempt to interpret them as their Unicode conversion
+        assertEquals("\\x4C",latinCapsL.toUTF8StringWithoutQuotes());
+        assertEquals("\\xE2\\xB1\\xAC",latinSmallLetterZWithDescender.toUTF8StringWithoutQuotes());
+        assertEquals("\\xF0\\x9D\\x84\\xBD",musicalSymbolQuarterRest.toUTF8StringWithoutQuotes());
     }
 
     @Test
     public void testGetCharacterName() throws Exception {
-        assertTrue("LATIN CAPITAL LETTER L".equalsIgnoreCase(latinCapsL.getCharacterName()));
-        assertTrue("LATIN SMALL LETTER Z WITH DESCENDER".equalsIgnoreCase(latinSmallLetterZWithDescender.getCharacterName()));
-        assertTrue("MUSICAL SYMBOL QUARTER REST".equalsIgnoreCase(musicalSymbolQuarterRest.getCharacterName()));
+        assertEquals("LATIN CAPITAL LETTER L",latinCapsL.getCharacterName());
+        assertEquals("LATIN SMALL LETTER Z WITH DESCENDER",latinSmallLetterZWithDescender.getCharacterName());
+        assertEquals("MUSICAL SYMBOL QUARTER REST",musicalSymbolQuarterRest.getCharacterName());
     }
 }
--- src/edu/imhoffc/Main.java
+++ src/edu/imhoffc/Main.java
@@ -14,7 +14,6 @@ public class Main {
         System.out.println("Hey "+name);
         System.out.println();
         System.out.println();
-
     }
 
 }
