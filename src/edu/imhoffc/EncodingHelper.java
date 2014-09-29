f99e40464a25ea51b6874015e3bac3ddc267872a
Charlie Imhoff <imhoffc@carleton.edu>
2014-09-29 08:32:03 -0500

    Renamed 'Main' to 'EncodingHelper' so that you can run it in terminal as 'java EncodingHelper' instead of 'java Main '

--- src/edu/imhoffc/EncodingHelper.java
+++ src/edu/imhoffc/EncodingHelper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class EncodingHelper {
 
     public static void main(String[] args) throws Exception {
 
--- src/edu/imhoffc/tester.java
+++ src/edu/imhoffc/tester.java
@@ -7,7 +7,7 @@ public class tester {
     public static void main(String[] args) {
         String[] thingy = {"--inputtype=utf8","--outputtype=STRING","'\\xC3\\xAA\\x74\\x72\\x65'"} ;
         try {
-            Main.main(thingy);
+            EncodingHelper.main(thingy);
         } catch (Exception e) {
             e.printStackTrace();
         }
